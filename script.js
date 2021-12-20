76509c2bba801fb26fe35df56f8b99b30a91529b
Brandon Chu <brandoc318@gmail.com>
2021-12-20 15:23:29 -0800

    Added event listeners for each number button and created temporary calculator styling.

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ function divide (num1, num2) {
 function operate (num1, num2) {
     divide(num1 / num2);
 }
+
+const numbers = document.querySelectorAll('.num-btn');
+const display = document.querySelector('#display');
+let displayValue = '';
+
+numbers.forEach((btn) => {
+    btn.addEventListener('click', () =>{
+        const num = document.createElement('div');
+        num.textContent = btn.textContent;
+        displayValue += num.textContent;
+        display.appendChild(num);
+        console.log(displayValue)
+    });
+});
