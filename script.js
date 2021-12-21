c0717d29ec1c2ad5f0fa0845b52316b3e7f462af
Brandon Chu <brandoc318@gmail.com>
2021-12-20 23:29:20 -0800

    Clear btn and calculator now works for 1 operation. Working on chaining the operations.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function add(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
 
 function sub(num1, num2) {
-  return num1 - num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+    return num1 / num2;
 }
 
 function operate(op, num1, num2) {
-  switch (op) {
-    case "÷":
-      return divide(num1, num2);
-    case "x":
-      return multiply(num1, num2)
-    case "-":
-      return sub(num1, num2);
-    case "+":
-      return add(num1,num2);
-  }
+    switch (op) {
+        case "÷":
+            return divide(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "-":
+            return sub(num1, num2);
+        case "+":
+            return add(num1, num2);
+    }
 }
 
 const numbers = document.querySelectorAll(".num-btn");
@@ -33,49 +33,56 @@ let displayValue = "";
 let displayArray = [];
 
 numbers.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const num = document.createElement("div");
-    num.textContent = btn.textContent;
-    displayValue += num.textContent;
-    displayArray.push(num);
-    display.appendChild(num);
-  });
+    btn.addEventListener("click", () => {
+        const num = document.createElement("div");
+        num.textContent = btn.textContent;
+        displayValue += num.textContent;
+        displayArray.push(num);
+        display.appendChild(num);
+    });
 });
 
 const operations = document.querySelectorAll(".op-btn");
 let value1;
 let op;
+let counter = 0;
 
 operations.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    value1 = operator(displayValue);
-    op = btn.textContent;
-  });
+    btn.addEventListener("click", () => {
+        value1 = operator(displayValue);
+        op = btn.textContent;
+    });
 });
 
 function operator(value1) {
-  clear();
-  return value1;
+    clear();
+    return value1;
 }
 
 function clear() {
-  for (let i = 0; i < displayValue.length; i++) {
-    display.removeChild(displayArray[i]);
-  }
-  displayArray = [];
-  displayValue = "";
+    for (let i = 0; i < displayArray.length; i++) {
+        display.removeChild(displayArray[i]);
+    }
+    displayArray = [];
 }
 
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", () => {
-  clear();
+    clear();
+    displayValue = "";
 });
 
 const equalsBtn = document.querySelector("#equals");
 equalsBtn.addEventListener("click", () => {
-  const sol = document.createElement('div');
-  sol.textContent = operate(op, value1, displayValue);
-  clear();
-  display.appendChild(sol);
-  displayArray.push(sol);
+    calculate();
 });
+
+function calculate() {
+    const sol = document.createElement("div");
+    sol.textContent = operate(op, value1, displayValue);
+    console.log(`${value1} ${displayValue}`);
+    clear();
+    display.appendChild(sol);
+    displayValue = sol.textContent;
+    displayArray.push(sol);
+}
