43f389facfe4e75a6054e4928097b11c06a11f84
Brandon Chu <brandoc318@gmail.com>
2021-12-21 22:25:55 -0800

    Chaining operations now seems to work. Bug testing, minor changes, styling, and EC is left.

--- script.js
+++ script.js
@@ -31,11 +31,16 @@ const numbers = document.querySelectorAll(".num-btn");
 const display = document.querySelector("#display");
 let displayValue = "";
 let displayArray = [];
+let value1;
+let value2;
+let op;
+let count = 0;
+let afterOP = false;
 
 numbers.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if (afterOP = true) {
-            clearDisplay();
+        if (afterOP == true) {
+            clearDisplay(false);
             afterOP = false;
         }
         const num = document.createElement("div");
@@ -48,11 +53,6 @@ numbers.forEach((btn) => {
 
 const operations = document.querySelectorAll(".op-btn");
 
-let value1;
-let value2;
-let op;
-let afterOP = false;
-
 operations.forEach((btn) => {
     btn.addEventListener("click", () => {
         getValue1();
@@ -60,29 +60,49 @@ operations.forEach((btn) => {
     });
 });
 
-function getValue1 () {
-    value1 = displayValue;
-    displayValue = '';
+function getValue1() {
+    if (count >= 1) {
+        value1 = calculate();
+    } else {
+        value1 = displayValue;
+    }
+    count++;
+    displayValue = "";
     afterOP = true;
 }
 
-const equalsBtn = document.querySelector('#equals');
-equalsBtn.addEventListener('click', () => {
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", () => {
     calculate();
 });
 
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", () => {
+    clearDisplay(true);
+});
+
 function calculate() {
+    console.log(displayValue);
     value2 = displayValue;
-    clearDisplay();
-    let sol = document.createElement('div');
+    clearDisplay(true);
+    let sol = document.createElement("div");
     sol.textContent = operate(op, value1, value2);
+    console.log(value1 + " " + value2);
+    displayArray.push(sol);
     console.log(sol.textContent);
+    displayValue = sol.textContent;
     display.appendChild(sol);
+    afterOP = true;
+    return sol.textContent;
 }
 
-function clearDisplay() {
+function clearDisplay(reset) {
     for (let i = 0; i < displayArray.length; i++) {
         display.removeChild(displayArray[i]);
     }
+    displayValue = '';
     displayArray = [];
+    if (reset == true) {
+        count = 0;
+    } 
 }
